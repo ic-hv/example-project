f6f38b80585c4b7416bcf98859cb59bddd25fe0f
Heiko Vogel <vogel@intercorp.de>
2021-07-16 09:37:35 +0200

    [TASK] Stammtisch-Zeitpunkt ergänzen

--- src/classes/Stammtisch.php
+++ src/classes/Stammtisch.php
@@ -13,9 +13,19 @@ class Stammtisch {
       $this->teilnehmer[] = new Teilnehmer($name);
    }
 
+   public static function getTimeString() {
+      return (new DateTime())->format(DATE_ATOM);
+   }
+
    public function greeting() {
+<<<<<<< HEAD
       foreach($this->teilnehmer as $teilnehmer) {
          echo 'Sei gegrüßt, ' . $teilnehmer->getName() . "\n";
+=======
+      echo 'Zeitpunkt des Stammtischs: ' . self::getTimeString() ."\n\n";
+      for($i=1; $i<=$this->teilnehmer; $i++) {
+         echo 'Sei gegrüßt, Interkörper ' . $i . "\n";
+>>>>>>> 9e3a96e... [TASK] Stammtisch-Zeitpunkt ergänzen
       }
    }
 }
